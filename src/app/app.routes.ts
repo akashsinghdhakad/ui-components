82caf0915ab678f448640574ea7cb633f10b5780
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-30 18:14:47 +0530

    sign up component and resolving bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
 
 export const routes: Routes = [
     // { path: '', component: LoginPageComponent },
-    { path: 'login', component: LoginPageComponent }
+    { path: 'login', component: LoginPageComponent },
+    {path : 'sign-up', component : SignupPageComponent}
 ];
