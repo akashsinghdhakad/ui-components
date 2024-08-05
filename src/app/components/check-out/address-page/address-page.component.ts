b0b95cfde9467e3214ce6ca449428b7ef519bed8
Akash dhakad <akashsinghdhakad@gmail.com>
2024-08-05 23:36:23 +0530

    unstable payment pages

---
+++ src/app/components/check-out/address-page/address-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-address-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './address-page.component.html',
+  styleUrl: './address-page.component.css'
+})
+export class AddressPageComponent {
+
+}
